c66e02352b5dfb6d14412197083d4c469ee7da74
Gilson Filho <me@gilsondev.in>
2022-02-23 11:15:43 -0300

    Send subscription and email confirmation

--- README.md
+++ README.md
@@ -33,14 +33,29 @@ make heroku-deploy
 
 ## Scenarios
 
-1. Create a subscription
+1. Access Subscription form page
 
 ```gherkin
 Dado que temos um visitante qualquer
-Quando ele acessa o endereço **/inscricao/**
+Quando ele acessa o endereço /inscricao/
 Então ele vê a página de inscrição
   E a página possui um formulário
   E o formulário possui 4 campos
   E os campos são nome, email, cpf e telefone
   E o formulário possui um botão para inscrever.
+```
+
+2. Create Subscription
+
+```gherkin
+Dado que um visitante acessa /inscricao/
+Quando ele preencher o formulário
+  E nome, cpf, email e telefone são informados
+  E ele clica em Enviar
+Então o sistema envia um email de confirmaçãordered list
+  E o remetente é contato@eventex.com.br
+  E o destinatário é o visitante
+  E o remetente está em cópia carbono
+  E o visitante é redirecionado para /inscricao/
+  E o visitante vê uma mensagem de sucesso.
 ```
--- eventex/subscriptions/views.py
+++ eventex/subscriptions/views.py
@@ -1,8 +1,33 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.core import mail
+from django.shortcuts import redirect, render
+from django.template.loader import render_to_string
 
 from eventex.subscriptions.forms import SubscriptionForm
 
 
 def subscribe(request):
-    context = {"form": SubscriptionForm()}
-    return render(request, "subscriptions/subscription_form.html", context)
+    if request.method == "POST":
+        form = SubscriptionForm(request.POST)
+        if form.is_valid():
+            context = {
+                "name": "Carlos Silva",
+                "cpf": "12345678901",
+                "email": "carlos.silva@mail.com",
+                "phone": "61 99876-1018",
+            }
+            body = render_to_string("subscriptions/subscription_email.txt", form.cleaned_data)
+
+            mail.send_mail(
+                "Confirmação de inscrição",
+                body,
+                "contato@eventex.com.br",
+                ["contato@eventex.com.br", form.cleaned_data["email"]],
+            )
+            messages.success(request, "Inscrição realizada com sucesso")
+            return redirect("/inscricao/")
+        else:
+            return render(request, "subscriptions/subscription_form.html", {"form": form})
+    else:
+        context = {"form": SubscriptionForm()}
+        return render(request, "subscriptions/subscription_form.html", context)
